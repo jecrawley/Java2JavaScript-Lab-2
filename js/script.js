958f7f4efda91619243418f2aaee94d46e37d960
James Crawley <jamescrawley@James-ZCW-15.local>
2016-09-29 20:21:47 -0400

    Added Dog, Cat and Bird as subtypes of Pet.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 " use strict ";
 
+function Pet (name) {
+
+    this.name = name;
+
+    this.speak = function () {
+        return "I am a pet called: " + name;
+    }
+
+};
+
+Dog.prototype = new Pet (name);
+Cat.prototype = new Pet (name);
+Bird.prototype = new Pet (name);
+
+function Dog (name) {
+    this.speak = function () {
+          return "Woof"
+    };
+};
+
+function Cat (name) {
+    this.speak = function () {
+          return "Meow"
+    };
+};
+
+function Bird (name) {
+    this.speak = function () {
+          return "Squawk"
+    };
+};
+
 function PetChat(){
   var display = document.getElementById("display");
   var numPets = null;
@@ -12,8 +44,8 @@ function PetChat(){
     for(var i=0; i<numPets; i++){
       var type = prompt("What is pet #" + (i+1) + "? (cat, dog, bird)");
       var name = prompt("What is pet #" + (i+1) + "'s name'?");
-      var textOut = type + " " + name + "<br />";
-      display.innerHTML += textOut;
+      var textOut = type + " " + name + "<br />"
+      display.innerHTML += textOut.speak();
     }
   };
 
